e24abd42045d6079fc8915230fb719e0c76d58e1
jokercsi <>
2021-05-29 08:47:54 +0000

    part 1 finish

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -66,15 +66,22 @@ assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 # c = color
 # colormap = color scheme 
 # edgecolor = making edge in element
-data.plot(kind='scatter', x="x", y="y", s=50, c = colors, colormap='rainbow', edgecolor='black') 
+ax = data.plot(kind='scatter', x="x", y="y", s=50, c = colors, colormap='rainbow', edgecolor='black') 
 
 # Add labels and title
 # YOUR CODE HERE 4
+import matplotlib.pyplot as plt
+
+plt.xlabel("x label")
+plt.ylabel("y label")
+plt.title('title')
+
 
 # Save the plot as a png file:
 outputfp = "my_first_plot.png"
 
 # YOUR CODE HERE 5
+plt.savefig(outputfp)
 
 # This test print statement should print the output filename of your figure
 print("Saved my first plot as:", outputfp)
